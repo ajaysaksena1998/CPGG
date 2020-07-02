681b82ccf3a01578016e4207ac723ac48400a6f0
ajaysaksena1998 <ajaysaksena1998@gmail.com>
2020-07-02 16:09:57 +0530

    Integrated

--- Automation_System/src/main/java/com/Ajay/Automate/jwt/resource/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.Ajay.Automate.jwt.resource;
-
-
-public class AuthenticationException extends RuntimeException {
-	public AuthenticationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
-
